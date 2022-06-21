c31501a0b0493aacb4be6b94a8e15663a239189c
shijintao <wb_shijintao@kuaishou.com>
2022-06-21 15:55:53 +0800

    Initial commit

---
+++ src/main/java/online/maxiaobai/MxbOnlineApplication.java
@@ -0,0 +1,13 @@
+package online.maxiaobai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MxbOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MxbOnlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/online/maxiaobai/MxbOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package online.maxiaobai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MxbOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
